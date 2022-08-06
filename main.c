d8fa019c7ae9116ae3fce5e2ee3ecc2261031025
drishtant5828 <85034759+drishtant5828@users.noreply.github.com>
2022-08-06 11:59:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b;
+   printf("enter the two numbers");
+   scanf("%d  %d", &a,&b);
+   int sum=a+b;
+   printf("%d + %d = %d",a,b,sum);
+   return 0;
+}
